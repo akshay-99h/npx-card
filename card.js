e9c08a0625d3dc423d82084a34314441d1de8a53
Akshay Prabhat Mishra <akshaypmishra02@gmail.com>
2023-07-16 23:00:54 +0530

    change import to require

--- card.js
+++ card.js
@@ -11,7 +11,7 @@ const fs = require("fs");
 const path = require("path");
 const ora = require("ora");
 const cliSpinners = require("cli-spinners");
-const fetch = import("node-fetch");
+const fetch = require("node-fetch");
 clear();
 
 const prompt = inquirer.createPromptModule();
